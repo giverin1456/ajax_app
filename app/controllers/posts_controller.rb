1ef09075b8344f6898439cb94221e11d76404b26
giverin1456 <give.taiki1456@gmail.com>
2021-03-17 20:51:21 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,21 @@ class PostsController < ApplicationController
     # メモを保存した後にトップページへリダイレクトする
   end
 
-  def show
-    @gibus = Post.find(1)
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      # 既読であるか否かを判定するプロパティを指定
+      post.update(checked: false)
+      # updateというActiveRecordのメソッドを使用して更新
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item}
+    # 最後に、更新したレコードをitem = Post.find(params[:id])で取得し直し、
+    # render json:{ post: item }でJSON形式（データ）としてchecked.jsに返却しています。
   end
 end
+
+# checkedアクション→既読の操作を行なった時に実行される
